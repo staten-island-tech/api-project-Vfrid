57bafca25d32de137b17ef1eb0653d6633ac4609
Vfrid <113192881+Vfrid@users.noreply.github.com>
2023-11-16 10:20:54 -0500

    blech

--- api-project/js/api.js
+++ api-project/js/api.js
@@ -1,7 +1,45 @@
-export const array = [
-    {
-        title: "jfdklas",
-        author: "jflkdsjalkfjds",
+import { DOMSelectors } from './main.js'
 
-    },
-]
+
+// export const array = [
+//     {
+//         title: "jfdklas",
+//         author: "jflkdsjalkfjds",
+
+//     },
+// ]
+
+// export const key = "1";
+// export function fetchResults(e) {
+//     // Use preventDefault() to stop the form submitting
+//     e.preventDefault();
+  
+//     // Assemble the full URL
+//     const url = `www.themealdb.com/api/json/v1/1/search.php?s=${DOMSelectors.search}`;
+
+//   }
+export const value = DOMSelectors.search.value
+// xhr.open('GET', `https://themealdb.com/api/json/v1/1/search.php?s=${value}`);
+
+
+// // Set the response type to JSON
+// xhr.responseType = 'json';
+
+
+// // Send the request
+// xhr.send();
+
+
+// // Define a callback function to handle the response
+// xhr.onload = function() {
+//   // Access the api data
+//   const response = xhr.response.response;
+//   const meal=response.strMeal
+
+
+//   // Do something with the response
+//   console.log(meal);
+// }
+
+
+
--- api-project/js/main.js
+++ api-project/js/main.js
@@ -1,25 +1,60 @@
 import '../styles/style.css'
-import { array } from './api.js'
+import { value } from './api.js'
 
-const DOMSelectors ={
+export const DOMSelectors ={
   app: document.querySelector('body'),
+  form: document.querySelector('#form'),
+  search: document.querySelector('.search')
 };
-function createcard(){
-  return array.forEach((thing)=> DOMSelectors.app.insertAdjacentHTML(
-    "afterbegin", 
-`<div class="box">
-    <div class="item">
-      <div class="item_link">
-        <div class="item_bg"></div>
 
-        <div class="item_title">
-          <p class="name_title">${thing.title} </p>
-        </div>
-          <span class="item_date"> ${thing.author}
-          </span>
-      </div>
-    </div>
-  </div>`
-  ))
-  }
-createcard();
+xhr.open('GET', `https://themealdb.com/api/json/v1/1/search.php?s=${value}`);
+
+
+// Set the response type to JSON
+xhr.responseType = 'json';
+
+
+// Send the request
+xhr.send();
+
+
+// Define a callback function to handle the response
+xhr.onload = function() {
+  // Access the api data
+  const response = xhr.response.response;
+  const meal=response.strMeal
+
+
+  // Do something with the response
+  console.log(meal);
+}
+
+function clearinput(){
+  value="";
+}
+
+// DOMSelectors.form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+
+//   createcard();
+//   clearinput();
+// });
+
+// function createcard(){
+//   return meals.forEach((thing)=> DOMSelectors.app.insertAdjacentHTML(
+//     "afterbegin", 
+// `<div class="box">
+//     <div class="item">
+//       <div class="item_link">
+//         <div class="item_bg"></div>
+
+//         <div class="item_title">
+//           <p class="name_title">${thing.name} </p>
+//         </div>
+//           <span class="item_date"> ${thing.ingredients}
+//           </span>
+//       </div>
+//     </div>
+//   </div>`
+//   ))
+//   }
