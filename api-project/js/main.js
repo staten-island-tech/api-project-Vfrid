d1b64ab7cea51b94757fbb31b62b195981013a80
Vfrid <113192881+Vfrid@users.noreply.github.com>
2023-11-21 10:20:40 -0500

    got cards?

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -7,43 +7,7 @@ export const DOMSelectors ={
   search: document.querySelector('#search')
 };
 
-
-function callAPI(e){
-  const request = new XMLHttpRequest();
-  const value = DOMSelectors.search.value
-  console.log(value)
-  request.open('GET', `https:themealdb.com/api/json/v1/1/search.php?s=${value}`);
-
-
-// Set the response type to JSON
-  request.responseType = 'json';
-
-
-// Send the request
-  request.send();
-
-
-// Define a callback function to handle the response
-  request.onload = function(e) {
-  // Access the api data
-    e.preventDefault();
-    const reply = request.response;
-    console.log(reply)
-    const recipes=reply.meals;
-    const meals = recipes.forEach((meal)=> console.log(meal.strMeal))
-
-  // Do something with the response
-    console.log(meals);
-    return (reply, recipes, meals);
-  }
-
-}
-
-
-
-function clearinput(){
-  DOMSelectors.search.value="";
-}
+const request = new XMLHttpRequest();
 
 function createcard(x){
   return x.forEach((thing)=> DOMSelectors.app.insertAdjacentHTML(
@@ -64,12 +28,47 @@ function createcard(x){
   ))
   }
 
+function callAPI(e){
+  const value = DOMSelectors.search.value
+  console.log(value)
+  request.open('GET', `https:themealdb.com/api/json/v1/1/search.php?s=${value}`);
+
+
+// Set the response type to JSON
+  request.responseType = 'json';
+
+
+// Send the request
+  request.send();
+
+
+// Define a callback function to handle the response
+  request.onload = function aa(e) {
+  // Access the api data
+    e.preventDefault();
+    const reply = request.response;
+    console.log(reply);
+    const recipes=reply.meals;
+    const meals = recipes.forEach((meal)=> console.log(meal.strMeal))
+
+  // Do something with the response
+    console.log(meals);
+    createcard(recipes);
+  }
+
+}
+
+
+function clearinput(){
+  DOMSelectors.search.value="";
+}
+
+
 
 DOMSelectors.form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   callAPI();
-  createcard(recipes);
   clearinput();
 });
 
