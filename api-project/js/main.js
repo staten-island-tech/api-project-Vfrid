37802ee0176f0179ed90c6940100f178f7b80092
Vfrid <113192881+Vfrid@users.noreply.github.com>
2023-11-27 10:18:28 -0500

    super broke lol

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -9,12 +9,11 @@ export const DOMSelectors ={
 
 const request = new XMLHttpRequest();
 
-function createcard(x){
-  
+function createeverythingcard(x){
   return x.forEach((thing)=> DOMSelectors.app.insertAdjacentHTML(
     "afterbegin", 
-`<a href=${thing.strSource}>
-<div class="box">
+  `<a href=${thing.strSource}>
+  <div class="box">
     <div class="item">
       <div class="item_link">
         <div class="item_bg"></div>
@@ -27,8 +26,63 @@ function createcard(x){
       </div>
     </div>
   </div>
-  </a>`
+  </a>`,
+  console.log('everything')
   ))
+}
+function createnosourcecard(x){
+  return x.forEach((thing)=> DOMSelectors.app.insertAdjacentHTML(
+    "afterbegin", 
+  `<a href=${thing.strYoutube}>
+  <div class="box">
+    <div class="item">
+      <div class="item_link">
+        <div class="item_bg"></div>
+
+        <div class="item_title">
+          <p class="name_title">${thing.strMeal} </p>
+        </div>
+          <span class="item_date"> ${thing.strCategory}
+          </span>
+      </div>
+    </div>
+  </div>
+  </a>`,
+  console.log('nosource')
+  ))
+}
+function createnothingcard(x){
+  return x.forEach((thing)=> DOMSelectors.app.insertAdjacentHTML(
+    "afterbegin", 
+  `<div class="box">
+    <div class="item">
+      <div class="item_link">
+        <div class="item_bg"></div>
+
+        <div class="item_title">
+          <p class="name_title">${thing.strMeal} </p>
+        </div>
+          <span class="item_date"> ${thing.strCategory}
+          </span>
+      </div>
+    </div>
+  </div>`,
+  console.log('nothing')
+  ))
+}
+function finalcreatecard(x){
+  x.forEach(function(thing){
+    if (thing.strSource !== null){
+    if (thing.strYoutube !== null){
+      createeverythingcard(x);
+    }
+    else {
+      createnosourcecard(x);
+    }}
+  else{
+    createnothingcard(x);
+  }
+})
   }
 
 function callAPI(e){
@@ -53,7 +107,7 @@ function callAPI(e){
     console.log(reply);
     const recipes=reply.meals;
   // Do something with the response
-    createcard(recipes);
+    finalcreatecard(recipes);
   }
 
 }
