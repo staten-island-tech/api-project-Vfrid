d65f4a2c93b88f7e56f7bfe1a336e0bd560960c2
Vfrid <113192881+Vfrid@users.noreply.github.com>
2023-11-28 10:20:13 -0500

    workin workin

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -4,55 +4,57 @@ import '../styles/style.css'
 export const DOMSelectors ={
   app: document.querySelector('#app'),
   form: document.querySelector('#form'),
-  search: document.querySelector('#search')
+  category_form: document.querySelector('#cat_form'),
+  search: document.querySelector('#search'),
+  category_search: document.querySelector('#category_search')
 };
 
 const request = new XMLHttpRequest();
 
 function createeverythingcard(x){
-  return x.forEach((thing)=> DOMSelectors.app.insertAdjacentHTML(
+  return DOMSelectors.app.insertAdjacentHTML(
     "afterbegin", 
-  `<a href=${thing.strSource}>
+  `<a href=${x.strSource}>
   <div class="box">
     <div class="item">
       <div class="item_link">
         <div class="item_bg"></div>
 
         <div class="item_title">
-          <p class="name_title">${thing.strMeal} </p>
+          <p class="name_title">${x.strMeal} </p>
+          <p class="goto"> Go To Source Recipe </p>
         </div>
-          <span class="item_date"> ${thing.strCategory}
+          <span class="item_date"> ${x.strCategory}
           </span>
       </div>
     </div>
   </div>
   </a>`,
-  console.log('everything')
-  ))
+  )
 }
 function createnosourcecard(x){
-  return x.forEach((thing)=> DOMSelectors.app.insertAdjacentHTML(
+  return DOMSelectors.app.insertAdjacentHTML(
     "afterbegin", 
-  `<a href=${thing.strYoutube}>
+  `<a href=${x.strYoutube}>
   <div class="box">
     <div class="item">
       <div class="item_link">
         <div class="item_bg"></div>
-
-        <div class="item_title">
-          <p class="name_title">${thing.strMeal} </p>
-        </div>
-          <span class="item_date"> ${thing.strCategory}
-          </span>
+          
+        <div class="item_title"> 
+          <p class="name_title">${x.strMeal} </p>
+          <p class="goto"> Go To Video Recipe </p>
+        </div> 
+          <span class="item_date"> ${x.strCategory}
+          </span> 
       </div>
     </div>
   </div>
   </a>`,
-  console.log('nosource')
-  ))
+  )
 }
 function createnothingcard(x){
-  return x.forEach((thing)=> DOMSelectors.app.insertAdjacentHTML(
+  return DOMSelectors.app.insertAdjacentHTML(
     "afterbegin", 
   `<div class="box">
     <div class="item">
@@ -60,32 +62,17 @@ function createnothingcard(x){
         <div class="item_bg"></div>
 
         <div class="item_title">
-          <p class="name_title">${thing.strMeal} </p>
+          <p class="name_title">${x.strMeal} </p>
         </div>
-          <span class="item_date"> ${thing.strCategory}
+          <span class="item_date"> ${x.strCategory}
           </span>
       </div>
     </div>
   </div>`,
-  console.log('nothing')
-  ))
+  )
 }
-function finalcreatecard(x){
-  x.forEach(function(thing){
-    if (thing.strSource !== null){
-    if (thing.strYoutube !== null){
-      createeverythingcard(x);
-    }
-    else {
-      createnosourcecard(x);
-    }}
-  else{
-    createnothingcard(x);
-  }
-})
-  }
 
-function callAPI(e){
+function callAPIsearch(e){
   const value = DOMSelectors.search.value
   console.log(value)
   request.open('GET', `https:themealdb.com/api/json/v1/1/search.php?s=${value}`);
@@ -107,11 +94,60 @@ function callAPI(e){
     console.log(reply);
     const recipes=reply.meals;
   // Do something with the response
-    finalcreatecard(recipes);
+    recipes.forEach(function(meal){
+    if (meal.strSource === null){
+      if (meal.strYoutube === null){
+        createnothingcard(meal);
+        console.log('nothing');
+      }
+      createnosourcecard(meal);
+      console.log('nosource : go to utube')
+    }
+    else {
+      createeverythingcard(meal);
+      console.log('everything')
+    }})
   }
 
 }
+function callAPIcat(e){
+  const category = DOMSelectors.category_search.value
+  console.log(category)
+  request.open('GET', `https:themealdb.com/api/json/v1/1/filter.php?c=${category}`);
 
+
+// Set the response type to JSON
+  request.responseType = 'json';
+
+
+// Send the request
+  request.send();
+
+
+// Define a callback function to handle the response
+  request.onload = function aa(e) {
+  // Access the api data
+    e.preventDefault();
+    const reply = request.response;
+    console.log(reply);
+    const recipes=reply.meals;
+  // Do something with the response
+    recipes.forEach(function(meal){
+    if (meal.strSource === null){
+      if (meal.strYoutube === null){
+        createnothingcard(meal);
+        console.log('nothing');
+      }
+      createnosourcecard(meal);
+      console.log('nosource : go to utube')
+    }
+    else {
+      createeverythingcard(meal);
+      console.log('everything')
+    }})
+  }
+
+}
 
 function clearinput(){
   DOMSelectors.search.value="";
@@ -122,7 +158,14 @@ function clearinput(){
 DOMSelectors.form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  callAPI();
+  callAPIsearch();
+  clearinput();
+});
+
+DOMSelectors.category_form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  callAPIcat();
   clearinput();
 });
 
