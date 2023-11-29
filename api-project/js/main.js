8052f7a1b2ac36a1e40c57c5e38375fb95a16ebe
Vfrid <113192881+Vfrid@users.noreply.github.com>
2023-11-29 10:20:49 -0500

    ugh

--- api-project/js/main.js
+++ api-project/js/main.js
@@ -6,7 +6,7 @@ export const DOMSelectors ={
   form: document.querySelector('#form'),
   category_form: document.querySelector('#cat_form'),
   search: document.querySelector('#search'),
-  category_search: document.querySelector('#category_search')
+  category_search: document.querySelector('#category_search'),
 };
 
 const request = new XMLHttpRequest();
@@ -71,24 +71,24 @@ function createnothingcard(x){
   </div>`,
   )
 }
-function createcatcard(x, y){
-  return DOMSelectors.app.insertAdjacentHTML(
-    "afterbegin", 
-  `<div class="box">
-    <div class="item">
-      <div class="item_link">
-        <div class="item_bg"></div>
+// function createcatcard(x, y){
+//   return DOMSelectors.app.insertAdjacentHTML(
+//     "afterbegin", 
+//   `<div class="box">
+//     <div class="item">
+//       <div class="item_link">
+//         <div class="item_bg"></div>
 
-        <div class="item_title">
-          <p class="name_title">${x.strMeal} </p>
-        </div>
-          <span class="item_category"> ${y}
-          </span>
-      </div>
-    </div>
-  </div>`
-  )
-}
+//         <div class="item_title">
+//           <p class="name_title">${x.strMeal} </p>
+//         </div>
+//           <span class="item_category"> ${y}
+//           </span>
+//       </div>
+//     </div>
+//   </div>`
+//   )
+// }
 function callAPIsearch(e){
   const value = DOMSelectors.search.value
   console.log(value)
@@ -130,7 +130,7 @@ function callAPIsearch(e){
 function callAPIcat(e){
   const category = DOMSelectors.category_search.value
   console.log(category)
-  request.open('GET', `https:themealdb.com/api/json/v1/1/search.php?s=`);
+  request.open('GET', `https:themealdb.com/api/json/v1/1/search.php?s`);
 
 
 // Set the response type to JSON
@@ -146,14 +146,21 @@ function callAPIcat(e){
   // Access the api data
     e.preventDefault();
     const reply = request.response;
-    console.log(reply);
+    // console.log(reply);
     const all_recipes=reply.meals;
-    recipes = all_recipes.filter((recipe)=> recipe.strCategory=== category)
+    const recipes = all_recipes.filter((recipe)=> recipe.strCategory=== category)
+    console.log(recipes)
   // Do something with the response
   //   recipes.forEach(function(meal){
   //   createcatcard(meal, category);
   // })
-  recipes.forEach(function(meal){
+  if (recipes.length === 0){
+    DOMSelectors.app.insertAdjacentHTML(
+      "afterbegin",
+      `<p class="no_cat_message"> There Are No Meals In This Category </p>`
+    )
+  }
+  else{recipes.forEach(function(meal){
     if (meal.strSource === null){
       if (meal.strYoutube === null){
         createnothingcard(meal);
@@ -166,13 +173,45 @@ function callAPIcat(e){
       createeverythingcard(meal);
       console.log('everything')
     }})
-  }
+  }}
+
+};
+
+function dropdown(e){
+  const new_req = new XMLHttpRequest();
+  new_req.open('GET', `https:themealdb.com/api/json/v1/1/categories.php`);
+  new_req.responseType = 'json';
+
+
+// Send the request
+new_req.send();
+
+
+// Define a callback function to handle the response
+new_req.onload = function aa(e) {
+  // Access the api data
+  e.preventDefault();
+  const reply = new_req.response;
+  console.log(reply);
+  const all_cats=reply.categories;
+// Do something with the response
+  all_cats.forEach((cat)=> DOMSelectors.category_search.insertAdjacentHTML(
+    'afterbegin',
+    `<option value=${cat.strCategory}>${cat.strCategory}</option>`
+  ))
+
+}
 
 }
 
 function callAPI(e){
-  request.open('GET', `https:themealdb.com/api/json/v1/1/search.php?s=`);
+  dropdown();
 
+  request.open('GET', `https:themealdb.com/api/json/v1/1/search.php?s=`);
+  // DOMSelectors.category_search.insertAdjacentHTML{
+  //   'afterbegin',
+  //   `<option value="volvo">Volvo</option>`
+  // }
 
 // Set the response type to JSON
   request.responseType = 'json';
@@ -204,7 +243,6 @@ function callAPI(e){
     console.log('everything')
   }})
 }
-
 }
 
 function clearinput(){
@@ -215,6 +253,7 @@ function clearinput(){
 function clearcards(){
   DOMSelectors.app.innerHTML = ""
 }
+
 
 callAPI();
 
